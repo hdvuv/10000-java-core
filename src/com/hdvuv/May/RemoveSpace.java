1573cd75c9beeffaab8d8d5ecc8a3b637581ec24
hdvuv <vuvdh.0308@gmail.com>
2022-05-24 23:52:30 +0700

    add RevertWord

--- src/com/hdvuv/May/RemoveSpace.java
+++ src/com/hdvuv/May/RemoveSpace.java
@@ -3,22 +3,22 @@ package com.hdvuv.May;
 /**
  * Remove spaces in string, includes space, \t, \n, etc
  */
-public class RemoveSpaceInString {
+public class RemoveSpace {
 
     /**
      * Solution 3 is high efficiency, it remove space, tab(\t), enter (\r\n), but not effective with full-space
      */
     public static void main(String[] args) {
         String inputText = "  abc  de gh j \t\n\r";
-        System.out.println("'" + RemoveSpaceInString_001(inputText) + "'");
-        System.out.println("'" + RemoveSpaceInString_002(inputText) + "'");
-        System.out.println("'" + RemoveSpaceInString_003(inputText) + "'");
+        System.out.println("'" + RemoveSpace_001(inputText) + "'");
+        System.out.println("'" + RemoveSpace_002(inputText) + "'");
+        System.out.println("'" + RemoveSpace_003(inputText) + "'");
     }
 
     /**
      * Using while combine with replace
      */
-    private static String RemoveSpaceInString_001(String input) {
+    private static String RemoveSpace_001(String input) {
         while (input.contains(" ") || input.contains("  ")) {
             input = input.replace(" ", "").replace("    ", "");
         }
@@ -28,7 +28,7 @@ public class RemoveSpaceInString {
     /**
      * Using replaceAll() with regex = " "
      */
-    private static String RemoveSpaceInString_002(String input) {
+    private static String RemoveSpace_002(String input) {
         return input.replaceAll(" ", "").replaceAll("\t", "");
     }
 
@@ -36,7 +36,7 @@ public class RemoveSpaceInString {
      * Using replaceAll() with regex = "\\s"
      * Note: "\\s" same result "\\s+"
      */
-    private static String RemoveSpaceInString_003(String input) {
+    private static String RemoveSpace_003(String input) {
         return input.replaceAll("\\s", "");
     }
 }
